15d5bd2afc52e060ee49cc216e2a8b0be19b9aa1
Jaeggler <gonzalo.santacruzm@gmail.com>
2019-12-04 16:31:29 -0500

    Push 0

---
+++ src/main/java/com/everis/noncorporate/NoncorporateApplication.java
@@ -0,0 +1,13 @@
+package com.everis.noncorporate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoncorporateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoncorporateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/noncorporate/NoncorporateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.noncorporate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoncorporateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
